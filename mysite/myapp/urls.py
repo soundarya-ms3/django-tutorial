ec8e6aa27101fb7f241285a54b46f8de287598e2
soundarya-ms3 <soundaryams03@gmail.com>
2023-01-12 19:10:16 +0530

    added code

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -7,8 +7,9 @@ app_name='myapp'
 urlpatterns = [
     path('',views.index),
     path('products/',views.products,name='products'),
-    path('products/<int:id>/',views.product_detail,name='product_detail'),
-    path('products/add/',views.add_product,name='add_product'),
-    path('products/update/<int:id>/',views.update_product,name='update_product'),
-    path('products/delete/<int:id>/',views.delete_product,name='delete_product'),
+    path('products/<int:pk>/',views.ProductDetailView.as_view(),name='product_detail'),
+    path('products/add/',views.ProductCreateView.as_view(),name='add_product'),
+    path('products/update/<int:pk>/',views.ProductUpdateView.as_view(),name='update_product'),
+    path('products/delete/<int:pk>/',views.ProductDelete.as_view(),name='delete_product'),
+    path('products/mylistings/',views.my_listings,name='my_listings'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Product
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from django.core.paginator import Paginator
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello worl")
 
 def products(request):
-    products = Product.objects.all()
+    page_obj=products = Product.objects.all()
+
+    product_name=request.GET.get('product_name')
+    if product_name!='' and product_name is not None:
+        page_obj=products.filter(name__icontains=product_name)
+
+    paginator=Paginator(page_obj,3)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
     context = {
-        'products':products
+        'page_obj':page_obj
     }
     return render(request,'myapp/index.html',context)
+
+# classs based view for above product view(ListView)
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'myapp/index.html'
+    context_object_name = 'products'
+    paginate_by = 3
+
+
 
 def product_detail(request,id):
     product=Product.objects.get(id=id)
@@ -19,17 +43,36 @@ def product_detail(request,id):
     }
     return render(request,'myapp/detail.html',context)
 
+# classs based view for above product_detail view(DetailView)
+
+class ProductDetailView(DetailView):
+    model=Product
+    template_name = 'myapp/detail.html'
+    context_object_name = 'product'
+
+
+@login_required
 def add_product(request):
     if request.method=='POST':
         name=request.POST.get('name')
         price=request.POST.get('price')
         desc=request.POST.get('desc')
         image=request.FILES['upload']
-        product=Product(name=name,price=price,desc=desc,image=image)
+        seller_name = request.user
+        product=Product(name=name,price=price,desc=desc,image=image,seller_name=seller_name)
         product.save()
-
-
     return render(request,'myapp/addproduct.html')
+
+# classs based view for above add_product view(CreateView)
+
+class ProductCreateView(CreateView):
+    model=Product
+    fields = ['name','price','desc','image','seller_name']
+    #product_form.html
+
+
+
+
 
 def update_product(request,id):
     product = Product.objects.get(id=id)
@@ -46,6 +89,16 @@ def update_product(request,id):
     }
     return render(request,'myapp/updateproduct.html',context)
 
+ #classs based view for above update_product view(UpdateView)
+class ProductUpdateView(UpdateView):
+    model=Product
+    fields = ['name','price','desc','image','seller_name']
+    template_name_suffix = '_update_form'
+    
+
+
+
+
 def delete_product(request,id):
     product = Product.objects.get(id=id)
     context = {
@@ -55,4 +108,16 @@ def delete_product(request,id):
         product.delete()
         return redirect('/myapp/products')
     return render(request,'myapp/delete.html',context)
-        
+#classs based view for above delete_product view(DeleteView)
+class ProductDelete(DeleteView):
+    model=Product
+    success_url = reverse_lazy('myapp:products')
+
+
+
+def my_listings(request):
+    products=Product.objects.filter(seller_name=request.user)
+    context={
+        'products':products,
+    } 
+    return render(request,'myapp/mylistings.html',context)  
